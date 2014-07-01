eb96274cb88c7173ac9be95e82f6d0264ecc023a
Paul Angell <angell.paul@gmail.com>
2014-07-01 17:08:44 +1000

    attempted selenium webdriver, and got results. But encountered some issues, and performs a little slow, and requires extra effort to run headless.

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ A script to retrieve the best days to buy a Megabus ticket.  Given a route,
 the program will respond with the 'best days' to buy a ticket.  'best days' is
 defined by the ticket cost of 1 pound and departure time before 1pm.
 
+Running with selenium
+[http://tutorials.jumpstartlab.com/topics/capybara/capybara_with_selenium_and_webkit.html]
+* xvfb-run [program ex. irb -I search.rb]
+
 * currently providing basic searching with predefined locations due to mechanize
   library
   for ruby not being able to handle javascript on the page.
--- search.rb
+++ search.rb
@@ -1,5 +1,35 @@
 
 require 'mechanize'
+require 'selenium-webdriver'
+
+################################################
+# A basic web scraper for the Megabus web interface
+#
+# It currently uses selenium webdriver to automate
+# the process of filling in the dynamic form.
+#
+# This is just a spike to try out selenium-webdriver.
+# Had some trouble when the form would update on selection,
+# when 'Element is no longer attached to the DOM'.
+# So I allowed for some time for the browser to update the
+# DOM.  I would rather have 'wait' blocks, but they
+# did not make a difference.  I suspect my check element
+# statement did not have the behaviour I was expecting.
+#
+# Headless selenium requires a bit more effort than
+# using a webkit enabled library.
+# Requirements:
+# firefox > 4
+# xvfb - to install see (http://tutorials.jumpstartlab.com/topics/capybara/capybara_with_selenium_and_webkit.html)
+# To run headless, in an irb session:
+# xvfb-run irb -r ./search.rb
+# MBD::example_run
+#
+# The ::example_run will search based on default values
+# 1 passenger, from: Southampton, to: London
+# outbound_date: "17/07/2014", return_date: "26/07/2014"
+# Then return the page source of the results to parse.
+################################################
 
 module MBD
   MEGABUS_URL = "http://uk.megabus.com/"
@@ -10,78 +40,111 @@ module MBD
     country: "JourneyPlanner$hdnSelected",
     outbound_date: "JourneyPlanner$txtOutboundDate",
     return_date: "JourneyPlanner$txtReturnDate",
-    leaving_from: "JourneyPlanner$ddlLeavingFromState",
+    country: "JourneyPlanner$ddlLeavingFromState",
+    leaving_from: "JourneyPlanner$ddlLeavingFrom",
     travelling_to: "JourneyPlanner$ddlTravellingTo",
-    travelling_by: "JourneyPlanner$ddlTravellingBy"}
+    travelling_by: "JourneyPlanner$ddlTravellingBy",
+    submit: "JourneyPlanner$btnSearch"}
 
-  class Agent
-    def initialize()
-      @agent = Mechanize.new()
-      @agent.user_agent_alias = "Mac Safari"
-      @agent.get(MEGABUS_URL)
-    end
+  def self.example_run
+    mbd = MBD::MegaBusDriver.new
+    mbd.set_number_of_passengers(1)
+    mbd.set_country("England")
+    mbd.set_leaving_from("Southampton")
+    mbd.set_travelling_to("London")
+    mbd.set_outbound_date
+    mbd.set_return_date
+    mbd.submit
+    mbd.results
   end
 
-  class Search
+  class MegaBusDriver
+    attr_accessor :driver
 
-    def initialize(agent, passengers)
-      @agent = agent
-      @passengers = passengers
+    def initialize
+      @driver = Selenium::WebDriver.for :firefox
+      @driver.manage.timeouts.implicit_wait = 10
+      @driver.navigate.to MEGABUS_URL
     end
 
-    def set_form
-
-    end
-
-    def set_number_passengers(num)
-      @agent.page.forms[0].fields.select {|e| e.name == MBD::FIELDS[:number_of_passengers]}.first.value = "1"
-    end
-
-    def set_travelling_from
-      @agent.page.forms.first.fields.select {|e| e.type = "hidden"}.map {|e| [e.name, e.value]}.reject {|e| e[1] == nil }.push ["__ASYNCPOST", "true"]
-    end
-
-    def set_outbound_date
-      @agent.page.form.fields.select {|e| e.name == MBD::FIELDS[:outbound_date]}.first.value = "17/07/2014"
-    end
-
-    def set_return_date
-      @agent.page.form.fields.select {|e| e.name == MBD::FIELDS[:return_date]}.first.value = "26/07/2014"
+    def set_number_of_passengers(num)
+      e = @driver.find_element(:name, MBD::FIELDS[:number_of_passengers])
+      e.clear
+      e.send_keys num
     end
 
     def list_countries
-      @agent.page.form.fields.select {|e| e.name == "JourneyPlanner$ddlLeavingFromState"}.first.options.map {|e| e.text}
+      @driver.find_elements( :tag_name => "option").map(&:text)
     end
 
-    #lists all locations, not grouped by country
+    def set_country(country = "England")
+      e = @driver.find_element(:name, MBD::FIELDS[:country])
+      e.find_elements( :tag_name => "option").find { |o| o.text == country}.click
+    end
+
+    def selected_country?
+      @driver
+        .find_elements( :tag_name => "option")
+        .map {|e| e if e.selected? }.compact[0].text
+    end
+
     def list_leaving_from
-      @agent.page.forms[0].fields.select {|e| e.name == "JourneyPlanner$ddlLeavingFrom" }.first.options.map {|e| e.text }[1..-1]
+      e = @driver
+        .find_element(:name, "JourneyPlanner$ddlLeavingFrom")
+        .find_elements( :tag_name => "option")
+    end
+
+    def set_leaving_from(from = "Southampton")
+      sleep 2
+      element = @driver
+        .find_element(:name, "JourneyPlanner$ddlLeavingFrom")
+        .find_elements( :tag_name => "option")
+        .find { |o| o.text == from }.click
     end
 
     def list_travelling_to
-      @agent.page.forms[0].fields.select {|e| e.name == "JourneyPlanner$ddlTravellingTo" }.first.options.map {|e| e.text }[1..-1]
+      @driver
+        .find_element(:name, MBD::FIELDS[:travelling_to])
+        .find_elements( :tag_name => "option")
+        .map(&:text)
     end
 
-    def choose_england()
-      @agent.page.form.fields.select {|e| e.name == "JourneyPlanner$ddlLeavingFromState"}.first.options[2].click
+    def set_travelling_to(to = "London")
+      sleep 2
+      @driver
+        .find_element(:name, MBD::FIELDS[:travelling_to])
+        .find_elements( :tag_name => "option")
+        .find { |o| o.text == to }
+        .click
     end
 
-    def choose_leaving_from(index)
-      @agent.page.forms[0].fields.select {|e| e.name == "JourneyPlanner$ddlLeavingFrom" }.first.options[index].click
+    def set_outbound_date(date = "17/07/2014")
+      sleep 4
+      e = @driver.find_element(:name, MBD::FIELDS[:outbound_date])
+      e.clear
+      e.send_keys date
+      e.send_keys :enter
     end
 
-    def choose_leaving_from(index)
-      @agent.page.forms[0].fields.select {|e| e.name == "JourneyPlanner$ddlTravellingTo" }.first.options[index].click
+    def set_return_date(date = "26/07/2014")
+      sleep 2
+      e =  @driver.find_element(:name, MBD::FIELDS[:return_date])
+      e.clear
+      e.send_keys date
+      e.send_keys :enter
     end
 
-    def choose_(index)
-      m.page.forms[0].fields.select {|e| e.name == "JourneyPlanner$ddlLeavingFrom" }.first.options[index].click
+    def submit
+      @driver.find_element(:name, MBD::FIELDS[:submit]).click
     end
-  end
-end
 
-class UI
+    def results
+      @driver.page_source
+    end
 
-  def initialize(number_of_passengers)
+    def quit
+      @driver.quit
+    end
+
   end
 end
